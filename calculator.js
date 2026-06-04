944fcb490ceb4c38c931b117df8ff3f5b9ed72af
RamzyCodes101 <abubakarsidiquetamim796@gmail.com>
2026-06-04 15:34:59 +0000

    Add Initial susu members data structure

---
+++ calculator.js
@@ -0,0 +1,9 @@
+let members = [
+    { name : "khalil", balance : 3200, isActive : true, contributions : 6, dailyDeposit : 50},
+    { name : "fawaz", balance : 3500, isActive : true, contributions : 7, dailyDeposit : 50},
+    { name : "mubarak", balance : 3260, isActive : false, contributions : 7, dailyDeposit : 50},
+    { name : "khalil", balance : 3800, isActive : false, contributions : 5, dailyDeposit : 50},
+    { name : "naomi", balance : 2200, isActive : true, contributions : 5, dailyDeposit : 50},
+    { name : "khalil", balance : 3210, isActive : true, contributions : 6, dailyDeposit : 50},
+    { name : "nasir", balance : 2300, isActive : false, contributions : 4, dailyDeposit : 50},
+]
